ef60479e988f76ee3e8dd82bab2c0d3f75eda569
TranNgocHoi729 <58330604+TranNgocHoi729@users.noreply.github.com>
2019-12-20 16:02:09 +0700

    update

--- src/sort/Sort.java
+++ src/sort/Sort.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sort;
+package add;
 
 /**
  *
@@ -16,74 +16,81 @@ public class Sort {
      */
     public static void main(String[] args) {
         Sort s = new Sort();
-        int[] a = {1,5,3,2,7,4,6};
+        int[] a = { 10, 5, 3, 2, 7, 4, 6, 5, 1};
         s.insertionSort(a);
-        s.prints(a);
+          s.prints(a);
     }
-    
-    public void bubbleSort(int[] a){
-        if(a.length <= 1){
+
+    public void bubbleSort(int[] a) {
+
+        if (a.length <= 1) {
             return;
-        }else{
+        } else {
             int max = a[0];
-            for (int j = a.length-1; j >= 0; j--) {
+            for (int j = a.length - 1; j >= 0; j--) {
                 for (int i = 0; i < j; i++) {
-                if(a[i+1] <a[i]){ 
-                    int x = a[i];
-                    a[i] = a[i+1];
-                    a[i+1] = x;
+                    if (a[i + 1] < a[i]) {
+                        int x = a[i];
+                        a[i] = a[i + 1];
+                        a[i + 1] = x;
+                    }
+                    if (a[i] > max) {
+                        max = a[i];
+                    }
+
                 }
-                if(a[i] > max){
-                    max = a[i];
-                }
-            }
             }
         }
     }
-    
-    public void selectionSort(int[] a){
-        if(a.length <= 1){
+
+    public void selectionSort(int[] a) {
+        if (a.length <= 1) {
             return;
-        }else{
-            for (int i = a.length-1; i >= 0; i--) {
+        } else {
+            for (int i = a.length - 1; i >= 0; i--) {
+
                 int max = i;
                 for (int j = 0; j < i; j++) {
-                    if(a[j] > a[max]){
+                    if (a[j] > a[max]) {
                         max = j;
-                    }           
+                    }
                 }
                 int x = a[i];
                 a[i] = a[max];
                 a[max] = x;
-                
+
             }
         }
     }
-    
-    public void insertionSort(int[] a){
-        if(a.length <= 1){
+
+    public void insertionSort(int[] a) {
+        if (a.length <= 1) {
             return;
-        }else{
+        } else {
             for (int i = 1; i < a.length; i++) {
-                System.out.println(i);
-                for(int k = i ; k>= 0; k--){
-                    if(a[k] < a[k-1]){
+
+                for (int k = i; k >= 0; k--) {
+                    if (k == 0) {
+                        break;
+                    } else if (a[k] < a[k - 1]) {
                         int x = a[k];
-                        a[k] = a[k-1];
-                        a[k-1] = x;
-                    }else{
+                        a[k] = a[k - 1];
+                        a[k - 1] = x;
+                    } else {
                         break;
                     }
+
                 }
+
             }
+
         }
     }
-    
-    
-    public void prints(int[] a){
+
+    public void prints(int[] a) {
         for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i]+" ");
+            System.out.print(a[i] + " ");
         }
     }
-    
+
 }
